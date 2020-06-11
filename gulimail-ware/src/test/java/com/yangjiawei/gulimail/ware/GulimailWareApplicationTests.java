aa4a1b0b8c727ab4add02a980b9e4e8f7e6deb1f
yangjiawei <yangjiawei@fshows.com>
2020-06-11 18:58:32 +0800

    项目构建

---
+++ gulimail-ware/src/test/java/com/yangjiawei/gulimail/ware/GulimailWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yangjiawei.gulimail.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailWareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
